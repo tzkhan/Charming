4cf0b095cc7c2c76bbdfe295123ccc29f4369f95
Tamim Khan <tzkhan10@gmail.com>
2020-05-20 19:47:38 +0100

    Add support for property setter helper method

---
+++ src/Charming.Types/Internal/PropertyBase.cs
@@ -0,0 +1,35 @@
+﻿namespace Charming.Types.Internal
+{
+    using System;
+
+    public abstract class PropertyBase
+    {
+        internal PropertyState State { get; set; }
+
+        internal static void SetProperties<T>(Action<T> setter, T properties)
+            where T : PropertyBase
+        {
+            var state = new PropertyState(properties);
+
+            setter?.Invoke(properties);
+
+            state.Reset();
+        }
+
+        protected internal T Get<T>(T property)
+            where T : new()
+        {
+            if (State is { } && property is null)
+            {
+                property = new T();
+
+                if (property is PropertyBase propertyBase)
+                {
+                    State.Handle(propertyBase);
+                }
+            }
+
+            return property;
+        }
+    }
+}
---
+++ src/Charming.Types/Internal/PropertyState.cs
@@ -0,0 +1,23 @@
+﻿namespace Charming.Types.Internal
+{
+    using System.Collections.Generic;
+
+    internal class PropertyState
+    {
+        private List<PropertyBase> _properties = new List<PropertyBase>();
+
+        public PropertyState(PropertyBase property) => Handle(property);
+
+        public void Handle(PropertyBase property)
+        {
+            property.State = this;
+            _properties.Add(property);
+        }
+
+        public void Reset()
+        {
+            _properties.ForEach(x => x.State = null);
+            _properties.Clear();
+        }
+    }
+}
